ca7cb011ac77159048455ff9d3ab1a9416cae0d8
irsa🍀 <sriirsaramayani@students.amikom.ac.id>
2026-07-28 03:51:58 +0700

    Update database.py

--- database.py
+++ database.py
@@ -4,11 +4,11 @@ from mysql.connector import Error
 def get_db_connection():
     try:
         connection = mysql.connector.connect(
-            host="localhost",
-            user="root",
-            password="",  #
-            database="db_robux",
-            port="3306"   
+            host="lijzak.h.filess.io",
+            user="db_robux_production",
+            password="96afcc7d2e0ffee27e7921b632c858d75ca85a03",  
+            database="db_robux_production",
+            port="3305"   
         )
         if connection.is_connected():
             return connection
